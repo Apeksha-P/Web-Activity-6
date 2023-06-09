c560ca8b35d5a266fcb03411b3331b7a5c766403
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-06-09 13:32:58 +0530

    Add JavaScript File

---
+++ cal.js
@@ -0,0 +1,6 @@
+function area(){
+    let b = parseFloat(document.getElementById("leg_b").value);
+    let h = parseFloat(document.getElementById("leg_h").value);
+    document.getElementById("result").value= 0.5*b*h;
+}
+
